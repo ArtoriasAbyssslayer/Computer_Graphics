a86b88277408be4e7d286b2b0eb1374c0e2b867b
Artorias <harry.filis@yahoo.gr>
2022-04-05 03:21:50 +0300

    commit

--- TriangleShading_Gouraugh-Flat/demo_flat.py
+++ TriangleShading_Gouraugh-Flat/demo_flat.py
@@ -1,26 +1,6 @@
-import numpy as np
-import cv2 as cv
-import matplotlib.pyplot as plt
-import os
 
 from tqdm import tqdm
 for i in tqdm(range(10)):
-    # get the working directory path to __location__ variable
-    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    d = os.path.join(__location__,'./GHW_assets/hw1.npy')
-    [verts2d,vcolors,faces,depth] = np.tolist(np.load(d,allow_pickle=True))
-    # X = np.load(d)
-    # np_load_old = X
-    # #modify the default parameters of np.load()
-    # np.load = lambda *a,**k: np_load_old(*a, allow_pickle=True,**k)
-    # #call load_data with allow_pickle implicitly set to True
-    #
-    # (verts2d,vcolors,faces,depth) = imdb.load_data(num_words=10000)
-    #
-    # # restore np.load from future normal usage
-    # np.load = np_load_old
-
-
 
 
 
