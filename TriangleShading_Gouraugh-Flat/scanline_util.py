f43292229b7ff30a25c2518062f07360243a7ecc
Artorias <harry.filis@yahoo.gr>
2022-04-10 16:18:03 +0300

    Create scanline utility functions for triangle filling and image rendering

--- TriangleShading_Gouraugh-Flat/scanline_util.py
+++ TriangleShading_Gouraugh-Flat/scanline_util.py
@@ -1,8 +1,9 @@
 import numpy as np
+import math
 import cv2 as cv
 import matplotlib.pyplot as plt
 import os
-
+import auxiliary_funcs as aux 
 
 class TriangleFillingFunction:
 	def __init__(self, return_value):
@@ -34,33 +35,41 @@ class TriangleFillingFunction:
 			gamma = abs(x2[0]-x[0])/abs(x2[0]-x1[0])
 			color_value = np.array(gamma*C1+(1-gamma)*C2)
 		else:
-			lambda1 = abs(x2[1]-x[1])/abs(x2[1]-x1[1])
-			lambda2 = abs(x2[0]-x[0])/abs(x2[0]-x1[0])
-			interpolation_arr = np.array([lambda1*C1 + (1-lambda1)*C2],[lambda2*C1 + (1-lambda2)*C2])
+
+			k1 = math.sqrt(pow(x1[0],2)+pow(x1[1],2))
+			k2 = math.sqrt(pow(x2[0],2)+pow(x2[1],2))
+			k3 = math.sqrt(pow(x[0],2)+pow(x[1],2))
+			lambda1 = abs(k2-k3)/abs(k1-k2)
+			lambda2 = abs(k1-k3)/abs(k1-k2)
+			d = lambda1*C2 + (1-lambda1)*C1
+			p = lambda2*C1 + (1-lambda2)*C2
+			# at this point I assume p==d but I will take their mean
+			interpolation_arr = np.array(d,p)
 			color_value = np.mean(interpolation_arr, axis=0)
-			
+
     # 1-gamma_x2 is the percentage of the colour similarity of x that is similar to point x2
     # This should be done on y axis as well for 2d point
-    # Now the itnerpolation can be done
-
-
-
-	def shade_triangle(self, img, verts2d, vcolors, shade_t):
-			# Triangle shading algorithm
-			''' Arguments
+    # Now the interpolation can be done
+	 # Triangle shading algorithm
+		""" 
+			Arguments
 			# 1. img : the image 2Dx3 with triangles
 			# 2. verts2d : interger matrix 3x2 [in each row contained the 2D coordinates of each vertex of the triagnle]
 			# 3. vcolors : matrix
-			'''
-			# get the working directory path to __location__ variable
-		    __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-		    d = os.path.join(__location__,'./GHW_assets/hw1.npy')
-			data = np.load(d,allow_pickle=True)
-			verts2d = data[()]['verts2d']
-			vcolors = data[()]['vcolors']
-			faces = data[()]['faces']
-			depth = data[()]['depth']
+			# 4. 
+			return_value:
+			- s_triangle : shaded triangle 
+
+		"""
+	def shade_triangle(self, img, verts2d, vcolors, shade_t):
+			# temporary variables
+			"""
+			 # 1. active_edges -> A list of 
+			 # 2. active_nodes ->
+
+			"""
 			if(shade_t = "gouraud"):
+				estimated_color = np.array(np.mean())
 
 			elif(shade_t = "flat"):
 
@@ -68,10 +77,33 @@ class TriangleFillingFunction:
 				print("Not valid shading algorithm.")
 			return Y
 
+
+	"""
+		render function colors every triangle in a loop using predetermined
+		Parameters:
+		- verts2D:  Lx2 matrix with the coordinates of every vertex of the graph
+		- faces: Kx3 matrix with the vertices of K triangles 
+		- depth: Lx1 matrix with the depth of every vertex in initial 3D space (before image is projected)
+		return value:
+		- img:	MxNx3 image with colors
+	"""
 	def render(verts2d,faces,vcolors,depth,shade_t):
 		if(shade_t != "flat") || (shade_t != "gouraud"):
 			print("Not valid shading algorithm")
 		if(m < 0 || n < 0):
 			print("Not valid graphic image dimensions")
-		'''Define image that will be rendered '''
-		img = np.ones((m,n,3))
+		'''Define a white image that will be rendered(filled with color) '''
+		img = np.ones((m,n,3));
+		for faces in faces:
+			for vertex in face:
+				triangle_depths[vertex] = depth[face]
+		sorted_depths = np.sort(depths,axis=0)
+		reversed_depths = sorted_depths[::-1]
+		# find vcolors for descending depths sorted triangles 
+		for st in reversed_depths:
+			v_colors_sorted_triang =  faces[st]
+			if(shade_t ==  'flat')
+				img = shade_triangle(img,reversed_depths,v_colors_sorted_triang,'flat');
+			if(shade_t == 'gouraud')
+				img = shade_triangle(img,reversed_depths,v_colors_sorted_triang,'gouraud');
+		return img
